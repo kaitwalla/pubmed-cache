d971dd6a2cc98595427de1e1dc68e4de165ee4f9
Kaitlyn Concilio <dan.herman@Gmail.com>
2017-01-12 19:02:07 -0500

    Update env.php
    Updated env variables

--- inc/env.php
+++ inc/env.php
@@ -1,7 +1,9 @@
 <?php
 
- $dsn = 'mysql:dbname=testdb;host=127.0.0.1';
- $user = 'dbuser';
- $password = 'password';
+$creds = new stdClass();
+
+$creds->dsn = 'mysql:dbname=testdb;host=127.0.0.1';
+$creds->user = 'dbuser';
+$creds->password = 'password';
 
 ?>
