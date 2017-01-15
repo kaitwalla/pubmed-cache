51312c55d1ac5436410a6e1120447ea12706fd26
Kaitlyn Concilio <dan.herman@Gmail.com>
2017-01-15 00:12:20 -0500

    Update env.php
    Added RSS description to env.php

--- inc/env.php
+++ inc/env.php
@@ -6,4 +6,5 @@ $creds->dsn = 'mysql:dbname=testdb;host=127.0.0.1';
 $creds->user = 'dbuser';
 $creds->password = 'password';
 
+$rss_description = "This is a PubMed Cache for a specific search";
 ?>
