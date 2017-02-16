b735be2eb634fcb983710ac059bc2f819152e425
Kaitlyn Concilio <kait@hey.com>
2017-02-16 13:27:13 -0500

    updated .env

--- inc/env.php
+++ inc/env.php
@@ -5,6 +5,7 @@ $creds = new stdClass();
 $creds->dsn = 'mysql:dbname=testdb;host=127.0.0.1';
 $creds->user = 'dbuser';
 $creds->password = 'password';
+$creds->security_token = 'REPLACE_THIS_WITH_A_RANDOM_STRING_TO_SECURE_AJAX';
 
 $rss_description = "This is a PubMed Cache for a specific search";
 ?>
